c061774f02b9b34f9d0ce2e7a09b94d69f5546ea
Julio Cabrera <juliocabrera820@gmail.com>
2020-08-22 08:52:53 -0500

    Initial commit

---
+++ ormconfig.ts
@@ -0,0 +1,8 @@
+export = {
+  type: process.env.TYPE,
+  host: process.env.HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+}
