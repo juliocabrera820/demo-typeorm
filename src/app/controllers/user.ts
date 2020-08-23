86e4ad36e7a44fdef3f55ff95ca1b749ca117c11
juliocabrera820 <juliocabrera820@gmail.com>
2020-08-23 11:00:56 -0500

    feat: update and delete user

--- src/app/controllers/user.ts
+++ src/app/controllers/user.ts
@@ -35,6 +35,32 @@ class UserController {
       return res.status(400).json(error)
     }
   }
+
+  async update (req: Request, res:Response):Promise<Response> {
+    const { id } = req.params
+    const { name, email } = req.body
+    try {
+      const user = await getRepository(User).findOne()
+      if (!user) {
+        return res.status(400).json({ message: 'user not found' })
+      }
+      getRepository(User).merge(user, { name, email })
+      await getRepository(User).save(user)
+      return res.status(200).json(user)
+    } catch (error) {
+      return res.status(400).json(error)
+    }
+  }
+
+  async delete (req:Request, res:Response):Promise<Response> {
+    const { id } = req.params
+    try {
+      await getRepository(User).delete(id)
+      return res.status(200).json({ message: 'user was deleted' })
+    } catch (error) {
+      return res.status(400).json(error)
+    }
+  }
 }
 
 export default new UserController()
--- src/routes.ts
+++ src/routes.ts
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/users', UserController.index)
 router.get('/users/:id', UserController.show)
 router.post('/users', UserController.create)
+router.put('/users/:id', UserController.update)
+router.delete('/users/:id', UserController.delete)
 
 export default router
