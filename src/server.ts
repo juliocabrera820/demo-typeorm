35414f849e1d7e023e3da4c319353f974918c72f
juliocabrera820 <juliocabrera820@gmail.com>
2020-08-30 18:17:47 -0500

    refactor: remove duplicated code closes #10

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
-import 'reflect-metadata'
 import app from './app'
 import dotenv from 'dotenv'
-import { createConnection } from 'typeorm'
 
 dotenv.config()
-createConnection()
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`)
